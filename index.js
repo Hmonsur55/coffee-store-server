ce165c5a62e400598bacf952edf025399631aa43
Hmonsur55 <116294687+Hmonsur55@users.noreply.github.com>
2023-05-09 22:10:55 +0600

    add deleted method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port =process.env.PORT || 5000
@@ -27,15 +27,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
-      
+        const coffeeCollection =client.db('coffeeDB').collection('coffee')
 
+    //   steps : 2 show data 
+      app.get('/coffee', async (req, res) => {
+          const cursor = coffeeCollection.find()
+          const result = await cursor.toArray()
+          res.send(result)
+})
+      
+    //  steps 1  receive data from server
+      app.post('/coffee', async (req, res) => {
+          const addCoffee = req.body;
+          console.log(addCoffee);
+          const result = await coffeeCollection.insertOne(addCoffee);
+          res.send(result);
+      })
+    //   for delete  
+      app.delete('/coffee/:id', async (req, res) => {
+          const id = req.params.id
+          const query = { _id: new ObjectId(id) }
+          const result = await coffeeCollection.deleteOne(query)
+          res.send(result);
+      })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
